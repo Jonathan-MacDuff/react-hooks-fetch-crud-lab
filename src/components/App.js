57c76da6b72e721c1327be206163eb1035ad101c
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-03-16 16:16:46 -0700

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,17 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  function newQuestionSubmission(newQuestion) {
+      const updatedQuestions = [...questions, newQuestion];
+      setQuestions(updatedQuestions);
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onSubmit={newQuestionSubmission}/> : <QuestionList questions={questions} setQuestions={setQuestions}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+
+
+function QuestionList({ questions, setQuestions }) {
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((data) => setQuestions(data))
+  }, []);
+
+  function deleteQuestion(q) {
+    const updatedQuestions = questions.filter((question) => question.id !== q.id);
+    setQuestions(updatedQuestions);
+  };
+
+  function changeAnswer(q) {
+    const updatedQuestions = questions.map((question) => {
+      if (q.id === question.id) {
+        return q;
+      } else {
+        return question;
+      }
+    });
+    setQuestions(updatedQuestions);
+  };
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => (
+        <QuestionItem 
+        key={question.id} 
+        question={question} 
+        onDelete={deleteQuestion}
+        onAnswerChange={changeAnswer}
+        />
+      ))}
+      </ul>
     </section>
   );
 }
